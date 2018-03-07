7e7f24da93b0365f7c197eada3631280a4656f3d
TristanJM <tristanjm96@gmail.com>
2018-03-07 16:49:49 +0000

    Trained best 6 models from GA

--- modules/const.py
+++ modules/const.py
@@ -7,29 +7,29 @@ TRAIN_SPLIT = 0.6
 VALIDATION_SPLIT = 0.2
 
 # Model
-NEURONS = [FEATURES, 10, 1] # Input, Hidden neurons, Output neurons
+NEURONS = [FEATURES, 8, 1] # Input, Hidden neurons, Output neurons
 LEARNING_RATE = 0.1
 MOMENTUM_ALPHA = 0.9
 MOMENTUM = True
 BOLD_DRIVER = False
 BOLD_DRIVER_INCREASE = 1.1  # Multiply LR by this amount if error is lower
 BOLD_DRIVER_DECREASE = 0.5
-MAX_EPOCHS = 200
+MAX_EPOCHS = 5000
 VALIDATION_EPOCHS = 100 # check validation set error every X epochs
 
 # Genetic Evol Algorithm
-GA = True
-GA_POP_SIZE = 12
-GA_GENERATIONS = 15
+GA = False
+GA_POP_SIZE = 28
+GA_GENERATIONS = 20
 GA_GENE_LEN = 12
-GA_BEST_INDIVIDUALS = 3
+GA_BEST_INDIVIDUALS = 6
 GA_CROSSOVER_PB = 0.4
 GA_MUTATION_PB = 0.1
 
 # Options
 TRAIN = True
 MODEL_DIR = './model/'
-MODEL_NAME = 'rXX'   # save model
+MODEL_NAME = 'r06'   # save model
 LOAD_MODEL = ''   # '' or model name
 
 RANDOM_SEED = 50
